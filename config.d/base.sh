283bc696660550325e8dd9e4e996befce5fe33db
Masahito Yoshida <masahito@axsh.net>
2015-03-12 22:07:34 +0900

    first commit

---
+++ config.d/base.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+# Do some changes ...
+
+user=${user:-vagrant}
+
+su - ${user} -c "bash -ex" <<'EOS'
+  deploy_to=/var/tmp/buildbook-rhel6
+
+  if ! [[ -d ${deploy_to} ]]; then
+    git clone https://github.com/wakameci/buildbook-rhel6.git ${deploy_to}
+  fi
+  cd ${deploy_to}
+  sudo ./run-book.sh jenkins.master
+  sudo ./run-book.sh hubot.common
+EOS
+
+chkconfig --list jenkins
+chkconfig jenkins on
+chkconfig --list jenkins
+
+service   jenkins start
